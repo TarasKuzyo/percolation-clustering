3d2a7fd3755be5ea7f4ed5b961defd3e84d42d47
Taras Kuzyo <kuzyo.taras@gmail.com>
2015-11-15 12:24:32 +0200

    created definitions.h

---
+++ definitions.h
@@ -0,0 +1,19 @@
+#ifndef __DEFINITIONS_H__
+#define __DEFINITIONS_H__
+
+#define SITE_BLOCK 0
+#define SITE_OPEN  1
+#define SITE_FULL  2
+
+
+typedef struct
+{
+    int width;
+    int height;
+    int *cells;
+
+} grid;
+
+
+
+#endif /* __DEFINITIONS_H__ */
